0c9c6f54b50a85b44740a358e918633b74c37cb5
Martijn Nagtegaal <m.a.nagtegaal@tudelft.nl>
2022-05-17 18:46:47 +0200

    edit __init__.py

--- MRFrecon/__init__.py
+++ MRFrecon/__init__.py
@@ -1,7 +1,7 @@
 from . import postprocessing as postprocessing
 from .backend import MrfData
 from .config import load_settings
-from .load_data import load_dict
+from .load_data import load_dict, load_data
 from .mrf_recon import recon_wavelet, recon_fbp, recon_sense, \
     recon_sense_into_spijn, recon_wavelet_into_spijn, \
     recon_direct_spijn, recon_admm_into_spijn, recon_lr_invert, \
